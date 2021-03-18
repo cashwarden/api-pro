fca50c281b3f67b7eb18ee46420a47b3dae7decc
forecho <caizhenghai@gmail.com>
2021-03-18 17:48:08 +0800

    fix: fix wechat login

---
+++ core/requests/WechatLogin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\core\requests;
+
+class WechatLogin extends \yii\base\Model
+{
+    public $code;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code'], 'trim'],
+            [['code'], 'required'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => t('app', 'Wechat Login Code'),
+        ];
+    }
+}
