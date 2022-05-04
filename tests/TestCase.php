dfc82504c2bf44fec3a7aca494d3df51e20c2b7e
forecho <caizhenghai@gmail.com>
2022-05-04 10:21:02 +0800

    tests: add test

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @author forecho <caizhenghai@gmail.com>
+ * @link https://cashwarden.com/
+ * @copyright Copyright (c) 2020-2022 forecho
+ * @license https://github.com/cashwarden/api/blob/master/LICENSE.md
+ * @version 1.0.0
+ */
+
+namespace tests;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    protected ?Client $http;
+
+    public function setUp(): void
+    {
+        $this->http = new Client(['base_uri' => 'https://localhost:8080']);
+    }
+
+    public function tearDown(): void
+    {
+        $this->http = null;
+    }
+}
